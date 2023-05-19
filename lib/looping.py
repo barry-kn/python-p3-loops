057ef2a597143f65f6e347b8623df24e77b05792
barry <barry.klein@student.moringaschool.com>
2023-05-19 13:04:19 +0300

    commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,42 @@
-#!/usr/bin/env python3
-
+ #####  happy_new_year
+####  pseudo code 
+### >>> count down 
+### while countdown is greater then 1 
+### output value of countdown 
+###  toa 1 from coutdown then at last happynew .......
+     
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    
+    while countdown > 1:
+        print(countdown)
+        countdown -= 2
+    
+    print("Happy New Year")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+happy_new_year()
+
+
+def square_integers(nums):
+    squared_nums = []
+    
+    for num in nums:
+        squared_nums.append(num ** 2)
+    
+    return squared_nums
+
+result = square_integers([1, 2, 3, 4, 5])
+print(result)
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+fizzbuzz()
